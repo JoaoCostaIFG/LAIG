c362551745f04698b6afba1fea8ed5bc808e7bfb
Ana Barros <up201806593@fe.up.pt>
2020-10-13 12:36:08 +0100

    Added MyNode comments

--- TP1/MyNode.js
+++ TP1/MyNode.js
@@ -1,16 +1,13 @@
 /**
  * MyNode
  * @constructor
+ * @param sceneGraph - Scene's Graph
+ * @param id - Node ID
  */
 class MyNode {
   constructor(sceneGraph, id) {
     this.id = id;
     this.sceneGraph = sceneGraph;
-
-    // if (dad != null) {
-      // this.tex = dad.getText();
-      // this.mat = dad.getMaterial();
-    // }
 
     this.tgMatrix = mat4.create();
 
@@ -18,50 +15,68 @@ class MyNode {
     this.descendantsLeaf = [];
   }
 
+  /**
+   * Multiplies matrix passed in function's arguments
+   * @param tg - transformation matrix to multiply 
+   */
   addTgMatrix(tg) {
-    // var result = [0, 0, 0, 0,
-    // 0, 0, 0, 0,
-    // 0, 0, 0, 0,
-    // 0, 0, 0, 0];
-
-    // for (var i = 0; i < 4; i++)
-    // for (var j = 0; j < 4; j++)
-    // for(var k = 0; k < 4; k++)
-    // result[i * 4 + j] += this.tgMatrix[i * 4 + k] * tg[k * 4 + j];
-
-    // this.tgMatrix = result;
-
     mat4.multiply(this.tgMatrix, this.tgMatrix, tg);
   }
 
+  /**
+   * Defines texture, afs and aft of node
+   * @param tex - texture 
+   * @param afs - texture amplification in S axis
+   * @param aft - texture amplification in T axis
+   */
   setTexture(tex, afs, aft) {
     this.tex = tex;
     this.afs = afs;
     this.aft = aft;
   }
 
+  /**
+   * Defines material of node
+   * @param mat - material to be set
+   */
   setMaterial(mat) {
     this.mat = mat;
   }
 
+  /**
+   * Adds descendants to node
+   * @param desc - descendent(s) to be added to node
+   */
   addDescendantNode(desc) {
     this.descendantsNode.push(desc);
   }
 
+  /**
+   * Adds descendants leaf to node
+   * @param desc - leaf descendent(s) to be added to node
+   */
   addDescendantLeaf(desc) {
     this.descendantsLeaf.push(desc);
   }
 
+  /**
+   * calls display on each CFGobject (leafs)
+   */
   displayPrimitives() {
-    // calls display on each CFGobject (leafs)
     this.descendantsLeaf.forEach(leaf => leaf.display());
   }
 
+  /**
+   * Displays node
+   */
   display() {
+    
     // transformations push
     this.sceneGraph.pushTransformation(this.tgMatrix);
+
     // materials push
     if (this.mat != "null") this.sceneGraph.pushMaterial(this.mat);
+
     // textures push
     if (this.tex != "null" && this.tex != "clear")
       this.sceneGraph.pushTexture(this.tex);
@@ -78,17 +93,25 @@ class MyNode {
     // textures pop
     if (this.tex != "null" && this.tex != "clear")
       this.sceneGraph.popTexture();
+
     // materials pop
     if (this.mat != "null")
       this.sceneGraph.popMaterial();
+
     // transformations pop
     this.sceneGraph.popTransformation();
   }
 
+  /**
+   * Enables normals visualization for each primitive
+   */
   enableNormalViz() {
     this.descendantsLeaf.forEach(leaf => leaf.enableNormalViz());
   }
 
+  /**
+   * Disables normals visualization for each primitive
+   */
   disableNormalViz() {
     this.descendantsLeaf.forEach(leaf => leaf.disableNormalViz());
   }
