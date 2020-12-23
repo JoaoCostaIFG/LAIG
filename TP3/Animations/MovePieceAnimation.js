935afe1cc6b47d77148fb89ff9f36a891729405e
Ana Barros <up201806593@fe.up.pt>
2020-12-23 20:58:28 +0000

    helpIrao

--- TP3/Animations/MovePieceAnimation.js
+++ TP3/Animations/MovePieceAnimation.js
@@ -1,7 +1,44 @@
-class MovePieceAnimation {
-  constructor() {}
+class MovePieceAnimation{
+    constructor(scene){
+        this.scene = scene;
 
-  update(t) {}
+        this.totalTime = 2;
+        this.isFinished = false;
+        this.sumT = 0;
+        this.lastTime = 0;
 
-  display() {}
+        this.genMatrix(0);
+    }
+
+    genMatrix(timePerc) {
+
+        let newZ = 10 * timePerc;
+
+        this.tgMatrix = mat4.create();
+        mat4.identity(this.tgMatrix);
+        mat4.translate(this.tgMatrix, this.tgMatrix, [0, 0, newZ]);
+    }
+
+    update(time){
+
+        if(this.isFinished) return;
+        if(this.sumT == 0)
+            this.initialTime = time;
+        
+        this.sumT += time - this.lastTime;
+        this.lastTime = time;
+
+        console.log(this.sumT);
+        if(this.sumT >= this.totalTime){
+            this.isFinished = true;
+            console.log("Finished Animation");
+        }
+
+        let timePerc = (this.sumT - this.initialTime)/this.totalTime;
+        this.genMatrix(timePerc);
+    }
+
+    display(){
+        this.scene.pushTransformation(this.tgMatrix);
+    }
 }
