8125e3dbb48777030b28279fe550f4a3f00d7f99
João Costa <up201806560@fe.up.pt>
2020-12-29 21:03:10 +0000

    Update: small code reoganization on MyPiece display.

--- TP3/Primitives/MyPiece.js
+++ TP3/Primitives/MyPiece.js
@@ -58,6 +58,88 @@ class MyPiece extends CGFobject {
     }
   }
 
+  displayWhiteInsignia() {
+    // Right
+    this.scene.pushMatrix();
+  
+    this.scene.translate(MyPiece.size/1.5, MyPiece.size/1.6, MyPiece.size/2.0);
+    this.scene.rotate(Math.PI / 2.0, 1, 0, 0);
+  
+    this.triangle.display();
+  
+    this.scene.popMatrix();
+  
+    // Down
+    this.scene.pushMatrix();
+  
+    this.scene.translate(MyPiece.size/2.0, MyPiece.size/1.6, MyPiece.size/1.5);
+    this.scene.rotate(Math.PI / 2.0, 1, 0, 0);
+    this.scene.rotate(Math.PI / 2.0, 0, 0, 1);
+  
+    this.triangle.display();
+  
+    this.scene.popMatrix();
+  
+    // Left
+    this.scene.pushMatrix();
+  
+    this.scene.translate(MyPiece.size/3.0, MyPiece.size/1.6, MyPiece.size/2.0);
+    this.scene.rotate(Math.PI / 2.0, 1, 0, 0);
+    this.scene.rotate(Math.PI, 0, 0, 1);
+  
+    this.triangle.display();
+  
+    this.scene.popMatrix();
+  
+    // Up
+    this.scene.pushMatrix();
+  
+    this.scene.translate(MyPiece.size/2.0, MyPiece.size/1.6, MyPiece.size/3.0);
+    this.scene.rotate(Math.PI / 2.0, 1, 0, 0);
+    this.scene.rotate(-Math.PI / 2.0, 0, 0, 1);
+  
+    this.triangle.display();
+  
+    this.scene.popMatrix();
+  }
+
+  displayBlackInsignia() {
+    this.scene.pushMatrix();
+
+    this.scene.translate(MyPiece.size/2.50, MyPiece.size/1.65, MyPiece.size/3.3);
+    this.scene.scale(MyPiece.size/11.0, MyPiece.size / 50.0, MyPiece.size/2.5);
+    this.cube.display();
+    
+    this.scene.popMatrix();
+
+    this.scene.pushMatrix();
+
+    this.scene.translate(MyPiece.size/2.0, MyPiece.size/1.65, MyPiece.size/3.3);
+    this.scene.scale(MyPiece.size/11.0, MyPiece.size / 50.0, MyPiece.size/2.5);
+    this.cube.display();
+    
+    this.scene.popMatrix();
+
+    // Circle
+    this.scene.pushMatrix();
+
+    this.scene.translate(MyPiece.size/3.25, MyPiece.size/1.60, MyPiece.size/2.0);
+    this.scene.rotate(Math.PI / 2.0, 1, 0, 0);
+    this.scene.scale(MyPiece.size/20.0, MyPiece.size /20.0, MyPiece.size/20.0);
+    this.cylinder.display();
+
+    this.scene.popMatrix();
+
+    this.scene.pushMatrix();
+
+    this.scene.translate(MyPiece.size/1.47, MyPiece.size/1.60, MyPiece.size/2.0);
+    this.scene.rotate(Math.PI / 2.0, 1, 0, 0);
+    this.scene.scale(MyPiece.size/20.0, MyPiece.size /20.0, MyPiece.size/20.0);
+    this.cylinder.display();
+
+    this.scene.popMatrix();
+  }
+
   display() {
     if (this.animation) this.animation.display();
 
@@ -98,90 +180,13 @@ class MyPiece extends CGFobject {
 
     this.scene.popMatrix();
 
-    if(this.color == Color.WHITE){
-      // Right
-      this.scene.pushMatrix();
-  
-      this.scene.translate(MyPiece.size/1.5, MyPiece.size/1.6, MyPiece.size/2.0);
-      this.scene.rotate(Math.PI / 2.0, 1, 0, 0);
-  
-      this.triangle.display();
-  
-      this.scene.popMatrix();
-  
-      // Down
-      this.scene.pushMatrix();
-  
-      this.scene.translate(MyPiece.size/2.0, MyPiece.size/1.6, MyPiece.size/1.5);
-      this.scene.rotate(Math.PI / 2.0, 1, 0, 0);
-      this.scene.rotate(Math.PI / 2.0, 0, 0, 1);
-  
-      this.triangle.display();
-  
-      this.scene.popMatrix();
-  
-      // Left
-      this.scene.pushMatrix();
-  
-      this.scene.translate(MyPiece.size/3.0, MyPiece.size/1.6, MyPiece.size/2.0);
-      this.scene.rotate(Math.PI / 2.0, 1, 0, 0);
-      this.scene.rotate(Math.PI, 0, 0, 1);
-  
-      this.triangle.display();
-  
-      this.scene.popMatrix();
-  
-      // Up
-      this.scene.pushMatrix();
-  
-      this.scene.translate(MyPiece.size/2.0, MyPiece.size/1.6, MyPiece.size/3.0);
-      this.scene.rotate(Math.PI / 2.0, 1, 0, 0);
-      this.scene.rotate(-Math.PI / 2.0, 0, 0, 1);
-  
-      this.triangle.display();
-  
-      this.scene.popMatrix();
-    }
-    else { // Black
-      this.scene.pushMatrix();
-
-      this.scene.translate(MyPiece.size/2.50, MyPiece.size/1.65, MyPiece.size/3.3);
-      this.scene.scale(MyPiece.size/11.0, MyPiece.size / 50.0, MyPiece.size/2.5);
-      this.cube.display();
-      
-      this.scene.popMatrix();
-
-      this.scene.pushMatrix();
-
-      this.scene.translate(MyPiece.size/2.0, MyPiece.size/1.65, MyPiece.size/3.3);
-      this.scene.scale(MyPiece.size/11.0, MyPiece.size / 50.0, MyPiece.size/2.5);
-      this.cube.display();
-      
-      this.scene.popMatrix();
-
-      // Circle
-      this.scene.pushMatrix();
-
-      this.scene.translate(MyPiece.size/3.25, MyPiece.size/1.60, MyPiece.size/2.0);
-      this.scene.rotate(Math.PI / 2.0, 1, 0, 0);
-      this.scene.scale(MyPiece.size/20.0, MyPiece.size /20.0, MyPiece.size/20.0);
-      this.cylinder.display();
-
-      this.scene.popMatrix();
-
-      this.scene.pushMatrix();
-
-      this.scene.translate(MyPiece.size/1.47, MyPiece.size/1.60, MyPiece.size/2.0);
-      this.scene.rotate(Math.PI / 2.0, 1, 0, 0);
-      this.scene.scale(MyPiece.size/20.0, MyPiece.size /20.0, MyPiece.size/20.0);
-      this.cylinder.display();
-
-      this.scene.popMatrix();
-    }
+    if(this.color == Color.WHITE)
+      this.displayWhiteInsignia();
+    else  // Black
+      this.displayBlackInsignia();
 
     this.scene.popTexture();
     this.scene.popMaterial();
     if (this.animation) this.scene.popTransformation();
-
   }
 }
